4bab1dd9cbe50325b2544400d3447b063fcf31b4
AlexMuhammad <76911219+AlexMuhammad@users.noreply.github.com>
2023-12-19 16:24:35 +0700

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,26 +1,18 @@
 import React, { useEffect, useState } from "react";
 
-const Loader = (props: { children: React.ReactNode }) => {
-  const [load, setLoad] = useState<boolean>(true);
+// interface LoaderProps {
+//   children: React.ReactNode;
+//   className?: string; // Make load prop optional
+// }
+
+const Loader = ({ children }: any) => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    setLoad(false);
-  });
+    setIsLoading(false);
+  }, []);
 
-  return (
-    <>
-      {load ? (
-        <div className="h-[50vh] space-x-1 flex justify-center items-center w-full">
-          <span className="sr-only">Loading...</span>
-          <div className="h-3 w-3 bg-red-500 rounded-full animate-bounce [animation-delay:-0.3s]"></div>
-          <div className="h-3 w-3 bg-red-500 rounded-full animate-bounce [animation-delay:-0.15s]"></div>
-          <div className="h-3 w-3 bg-red-500 rounded-full animate-bounce"></div>
-        </div>
-      ) : (
-        props.children
-      )}
-    </>
-  );
+  return <>{isLoading ? <span>loading</span> : <div>{children}</div>}</>;
 };
 
 export default Loader;
